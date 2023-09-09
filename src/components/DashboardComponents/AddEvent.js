a4287e1ad499e6d0f96e979db25b1a50956cc92a
rokydas <droky136@gmail.com>
2023-09-09 12:21:38 +0600

    made event description not required

--- src/components/DashboardComponents/AddEvent.js
+++ src/components/DashboardComponents/AddEvent.js
@@ -140,20 +140,16 @@ const AddEvent = () => {
               </span>
             )}
             <h6 className="text-secondary mt-3">
-              Description <span className="text-danger">*</span>
+              Description
             </h6>
             <textarea
               type="text"
               placeholder="Description"
               rows="7"
               className="form-control"
-              {...register("description", { required: true })}
+              {...register("description", { required: false })}
             />
-            {errors.designation && (
-              <span className="text-danger">
-                "Description" is not allowed to be empty
-              </span>
-            )}
+
             <h6 className="text-secondary mt-3">
               Gallery <span className="text-danger">*</span>
             </h6>
